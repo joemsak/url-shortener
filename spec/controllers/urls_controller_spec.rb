0e5c7fea713d909ce0cb4b81d56c003fd8c98dd8
Joe Sak <joe@joesak.com>
2014-08-30 10:12:22 -0400

    Fix URL factory usage

--- spec/controllers/urls_controller_spec.rb
+++ spec/controllers/urls_controller_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 describe UrlsController do
   describe 'GET #show' do
     context 'when the URL exists' do
-      let(:url) { build(:url) }
-
-      before { ShortUrl.create(url) }
+      let!(:url) { create(:url, long: 'http://longurl.com',
+                                shortened_hash: 'abc123') }
 
       it 'redirects to the long URL' do
-        get :show, id: url.shortened_hash
-        expect(response).to redirect_to(url.long)
+        get :show, id: 'abc123'
+        expect(response).to redirect_to('http://longurl.com')
       end
     end
 
@@ -25,7 +24,7 @@ describe UrlsController do
   describe 'POST #create' do
     context 'when the URL has been entered before' do
       it 'returns the duplicate url' do
-        url = create(:url)
+        url = create(:url, shortened_hash: 'foobar')
         post :create, url: { long: url.long }, format: :js
         expect(assigns[:url]).to eq(url)
       end
--- spec/factories/url.rb
+++ spec/factories/url.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :url do
     long 'http://something.com/valid'
+    shortened_hash 'abc12'
   end
 end
