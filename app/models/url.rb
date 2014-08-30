899250e35bf991547053e0371f3dc8f8b9aecd99
Joe Sak <joe@joesak.com>
2014-08-29 23:43:16 -0400

    Ensure URLs have http:// prepended

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,8 +1,16 @@
 class Url < ActiveRecord::Base
+  before_save :prepend_missing_http
+
   validates :long, presence: true,
     format: %r{\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\z}
 
   def shortened
     ShortUrl.for(self)
+  end
+
+  private
+  def prepend_missing_http
+    return true if long.match(/\Ahttp/)
+    long.insert(0, 'http://')
   end
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -7,4 +7,17 @@ describe Url do
       expect(url.shortened).to eq('http://example.com/foo')
     end
   end
+
+  describe 'before_save' do
+    context 'if http:// is missing from #long' do
+      it 'prepends http:// to #long' do
+        url = create(:url)
+        expect(url.long).to match(/http:\/\//)
+        expect(url.long).to_not match(/http:\/\/http:\/\//)
+
+        url = create(:url, long: 'google.com')
+        expect(url.long).to eq('http://google.com')
+      end
+    end
+  end
 end
